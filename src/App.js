9ff7c6273c743730ec2a85cd1738a34a59de3ad1
allanornel <allanornel@gmail.com>
2022-08-11 15:19:58 -0300

    feat: start of front end

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Timeline from "./pages/Timeline";
+import SignUp from "./pages/SignUp";
+import UserPage from "./pages/UserPage";
+import HashtagPage from "./pages/Hashtags";
+import { AuthProvider } from "./contexts/AuthContext";
+import SignIn from "./pages/SignIn";
+
+import "./assets/css/reset.css";
+import "./assets/css/style.css";
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/timeline" element={<Timeline />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<SignIn />} />
+          <Route path="/user/:user" element={<UserPage />} />
+          <Route path="/hashtag/:hashtag" element={<HashtagPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
